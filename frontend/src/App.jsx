cfff79d839c597ad20372c505c1374b9d60b2e30
ASHMITH <adhikamashmit@gmail.com>
2026-01-13 14:20:06 +0530

    Use env variable for API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
   const [userId, setUserId] = useState("");
